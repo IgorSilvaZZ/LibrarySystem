a18b5ed247584fe7bb443be508da883a707e0dfd
IgorSilvaZZ <igor06silva12@gmail.com>
2022-10-03 22:45:43 -0300

    Add librarian form

---
+++ frontend/react/library-system/src/components/Register/LibrarianForm/index.tsx
@@ -0,0 +1,7 @@
+export const LibrarianForm = () => {
+  return (
+    <div className='flex flex-col mx-10 h-4/5 gap-3 border border-blue-400'>
+      <div className='flex flex-col gap-2 h-16 w-3/4 border border-purple-400'></div>
+    </div>
+  );
+};
--- frontend/react/library-system/src/pages/Register/index.tsx
+++ frontend/react/library-system/src/pages/Register/index.tsx
@@ -1,48 +1,78 @@
-import { ArrowLeft } from "phosphor-react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "phosphor-react";
+
+import { Background } from "../../components/Background";
+import { LibrarianForm } from "../../components/Register/LibrarianForm";
 
 import openBookImage from "../../assets/open-book.png";
 import librarianImage from "../../assets/librarian.png";
 import bannerImage from "../../assets/footer-img2.png";
 
+export interface IRegisterTypeProps {
+  setChoiceType: (type: string) => void;
+}
+
 const Register = () => {
   const navigate = useNavigate();
+  const [typeChoice, setTypeChoice] = useState<string>("default");
+
+  function backTypeChoice() {
+    if (typeChoice === "librarian" || typeChoice === "user") {
+      setTypeChoice("default");
+    } else {
+      navigate("/login");
+    }
+  }
+
+  function handleChoice(type: string) {
+    setTypeChoice(type);
+  }
 
   return (
-    <div className=' flex justify-center items-center w-screen h-screen bg-white-container'>
+    <Background>
       <div className='flex h-[90%] w-[70%] bg-white rounded-xl'>
         <div className='flex items-center justify-center h-full w-[70%]'>
           <div className='flex justify-evenly flex-col w-full h-full'>
             <div className='flex flex-col px-12 text-3xl font-semibold text-pink-300'>
               <ArrowLeft
-                onClick={() => navigate("/login")}
+                onClick={backTypeChoice}
                 size={25}
                 color='#fea6b5'
                 className='mb-2 cursor-pointer font-semibold'
               />
 
-              <span>Escolha um tipo de conta</span>
+              {typeChoice === "default" && (
+                <span>Escolha um tipo de conta</span>
+              )}
             </div>
 
-            <div className='flex justify-center items-center h-3/5 w-full'>
-              <div className='flex flex-col justify-evenly items-center w-2/5 h-3/5 m-2 rounded-3xl text-white text-2xl font-bold cursor-pointer bg-pink-400 transition-all hover:w-[45%] hover:h-[65%]'>
-                <img
-                  className='w-2/6'
-                  src={openBookImage}
-                  alt='Item Reader Choose Register'
-                />
-                <span>Leitor</span>
-              </div>
+            {typeChoice === "default" && (
+              <div className='flex justify-center items-center h-3/5 w-full'>
+                <div className='flex flex-col justify-evenly items-center w-2/5 h-3/5 m-2 rounded-3xl text-white text-2xl font-bold cursor-pointer bg-pink-400 transition-all hover:w-[45%] hover:h-[65%]' onClick={() => handleChoice("user")}>
+                  <img
+                    className='w-2/6'
+                    src={openBookImage}
+                    alt='Item Reader Choose Register'
+                  />
+                  <span>Leitor</span>
+                </div>
 
-              <div className='flex flex-col justify-evenly items-center w-2/5 h-3/5 m-2 rounded-3xl text-white text-2xl font-bold cursor-pointer bg-pink-300 transition-all hover:w-[45%] hover:h-[65%]'>
-                <img
-                  className='w-2/6'
-                  src={librarianImage}
-                  alt='Item Librarian Choose Register'
-                />
-                <span>Bibliotecário</span>
+                <div className='flex flex-col justify-evenly items-center w-2/5 h-3/5 m-2 rounded-3xl text-white text-2xl font-bold cursor-pointer bg-pink-300 transition-all hover:w-[45%] hover:h-[65%]' onClick={() => handleChoice("librarian")}>
+                  <img
+                    className='w-2/6'
+                    src={librarianImage}
+                    alt='Item Librarian Choose Register'
+                  />
+                  <span>Bibliotecário</span>
+                </div>
               </div>
-            </div>
+            )}
+
+            {typeChoice === "librarian" && (
+              <LibrarianForm />
+            )}
+
           </div>
         </div>
 
@@ -53,7 +83,7 @@ const Register = () => {
           />
         </div>
       </div>
-    </div>
+    </Background>
   );
 };
 
